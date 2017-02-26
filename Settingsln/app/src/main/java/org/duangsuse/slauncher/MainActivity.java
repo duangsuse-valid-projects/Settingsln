c863810834905506e60603145fb069467c932a57
duangsuse <fedora-opensuse@outlook.com>
2017-02-26 11:09:19 +0800

    第一次

--- Settingsln/app/src/main/java/org/duangsuse/slauncher/MainActivity.java
+++ Settingsln/app/src/main/java/org/duangsuse/slauncher/MainActivity.java
@@ -2,6 +2,8 @@ package org.duangsuse.slauncher;
 
 import android.app.*;
 import android.os.*;
+import android.content.*;
+import android.widget.*;
 
 public class MainActivity extends Activity 
 {
@@ -9,6 +11,40 @@ public class MainActivity extends Activity
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-		finish();
+		
+		final EditText input = new EditText(this);
+		input.setHint(getString(R.string.hint));
+		input.setText("com.android.settings");
+		final Intent i = new Intent();
+		
+		new AlertDialog.Builder(this)
+			.setTitle("package?")
+			.setIcon(android.R.drawable.ic_media_play)
+			.setView(input)
+			.setPositiveButton(android.R.string.ok, 
+			new DialogInterface.OnClickListener() {
+				public void onClick(DialogInterface dialog , int c) {
+					startActi(i.setPackage(input.getText().toString()));
+					dialog.dismiss();
+				}
+			})
+			.setNegativeButton(android.R.string.cancel, 
+			new DialogInterface.OnClickListener() {
+				public void onClick(DialogInterface dialog , int c) {
+					dialog.dismiss();
+					finish();
+				}
+			})
+			.show();
     }
+	 void startActi(Intent i){
+		try{
+			startActivity(i);
+			finish();
+		} catch (Exception ActivityNotFoundException) {
+			Toast.makeText(this , "Activity Not Found" ,Toast.LENGTH_SHORT)
+				.show();
+				finish();
+		}
+	}
 }
